93d354d2a788ba912de21da1f7125e542a52f07a
Shahnawaz Shaikh <shahnawazshaikh67967@gmail.com>
2026-09-19 17:21:51 +0530

    fix: add INTERNET permission to AndroidManifest and fix care circle route precedence

--- backend/src/modules/care-circle/careCircle.routes.js
+++ backend/src/modules/care-circle/careCircle.routes.js
@@ -16,14 +16,13 @@ const {
 const CareCircleRouter = express.Router();
 
 CareCircleRouter.post('/', requireAuth, createCareCircle);
+CareCircleRouter.get('/users/:userId', requireAuth, getUserCareCircles);
+CareCircleRouter.get('/invitations/users/:userId', requireAuth, getUserInvitations);
 CareCircleRouter.get('/:id', requireAuth, requireCircleAccess('circle:view'), getCareCircle);
 CareCircleRouter.get('/:id/members', requireAuth, requireCircleAccess('circle:view'), listMembers);
 CareCircleRouter.post('/:id/invitations', requireAuth, requireCircleAccess('circle:manage_members'), inviteMember);
 CareCircleRouter.post('/invitations/accept', requireAuth, acceptInvitation);
 CareCircleRouter.patch('/:id/members/:memberId/revoke', requireAuth, requireCircleAccess('circle:revoke_member'), revokeMember);
 CareCircleRouter.delete('/:id', requireAuth, deleteCareCircle);
-
-CareCircleRouter.get('/users/:userId', requireAuth, getUserCareCircles);
-CareCircleRouter.get('/invitations/users/:userId', requireAuth, getUserInvitations);
 
 module.exports = CareCircleRouter;
